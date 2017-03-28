7a5ff16b09a2b865c574515b15c9aee7a6ef6cd2
redphyre <redphyre@gmail.com>
2017-03-28 10:35:04 -0500

    Proposed changes
    Check out this example of using [String: Any] dictionaries and using as! Type in your methods and loops to help make the code cleaner and result in less string to type conversions

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -3,24 +3,24 @@
 // Storing each players data in their own collection
 
 
-let player1: [String: String] = ["NAME": "Joe Smith", "HEIGHT": "42", "EXP": "Yes", "GUARDIAN": "Jim and Jan Smith"]
-let player2: [String: String] = ["NAME": "Jill Tanner", "HEIGHT": "36", "EXP": "Yes", "GUARDIAN": "Clara Tanner"]
-let player3: [String: String] = ["NAME": "Bill Bon", "HEIGHT": "43", "EXP": "Yes", "GUARDIAN": "Sara and Jenny Bon"]
-let player4: [String: String] = ["NAME": "Eva Gordon", "HEIGHT": "45", "EXP": "No", "GUARDIAN": "Wendy and Mike Gordon"]
-let player5: [String: String] = ["NAME": "Matt Gill", "HEIGHT": "40", "EXP": "No", "GUARDIAN": "Charles and Sylvia Gill"]
-let player6: [String: String] = ["NAME": "Kimmy Stein", "HEIGHT": "41", "EXP": "No", "GUARDIAN": "Bill and Hilary Stein"]
-let player7: [String: String] = ["NAME": "Sammy Adams", "HEIGHT": "45", "EXP": "No", "GUARDIAN": "Jeff Adams"]
-let player8: [String: String] = ["NAME": "Karl Saygan", "HEIGHT": "42", "EXP": "Yes", "GUARDIAN": "Heather Bledsoe"]
-let player9: [String: String] = ["NAME": "Suzane Greenberg", "HEIGHT": "44", "EXP": "Yes", "GUARDIAN": "Henrietta Dumas"]
-let player10: [String: String] = ["NAME": "Sal Dali", "HEIGHT": "41", "EXP": "No", "GUARDIAN": "Gala Dali"]
-let player11: [String: String] = ["NAME": "Joe Kavalier", "HEIGHT": "39", "EXP": "No", "GUARDIAN": "Sam and Elaine Kavalier"]
-let player12: [String: String] = ["NAME": "Ben Finkelstein", "HEIGHT": "44", "EXP": "No", "GUARDIAN": "Aaron and Jill Finkelstein"]
-let player13: [String: String] = ["NAME": "Diego Soto", "HEIGHT": "41", "EXP": "Yes", "GUARDIAN": "Robin and Sarika Soto"]
-let player14: [String: String] = ["NAME": "Chloe Alaska", "HEIGHT": "47", "EXP": "No", "GUARDIAN": "David and Jamie Alaska"]
-let player15: [String: String] = ["NAME": "Arnold Willis", "HEIGHT": "43", "EXP": "No", "GUARDIAN": "Claire Willis"]
-let player16: [String: String] = ["NAME": "Phillip Helm", "HEIGHT": "44", "EXP": "Yes", "GUARDIAN": "Thomas Helm and Eva Jones"]
-let player17: [String: String] = ["NAME": "Les Clay", "HEIGHT": "42", "EXP": "Yes", "GUARDIAN": "Wynonna Brown"]
-let player18: [String: String] = ["NAME": "Herschel Krustofski", "HEIGHT": "45", "EXP": "Yes", "GUARDIAN": "Hyman and Rachel Krustofski"]
+let player1: [String: Any] = ["NAME": "Joe Smith", "HEIGHT": 42, "EXP": true, "GUARDIAN": "Jim and Jan Smith"]
+let player2: [String: Any] = ["NAME": "Jill Tanner", "HEIGHT": 36, "EXP": true, "GUARDIAN": "Clara Tanner"]
+let player3: [String: Any] = ["NAME": "Bill Bon", "HEIGHT": 43, "EXP": true, "GUARDIAN": "Sara and Jenny Bon"]
+let player4: [String: Any] = ["NAME": "Eva Gordon", "HEIGHT": 45, "EXP": false, "GUARDIAN": "Wendy and Mike Gordon"]
+let player5: [String: Any] = ["NAME": "Matt Gill", "HEIGHT": 40, "EXP": false, "GUARDIAN": "Charles and Sylvia Gill"]
+let player6: [String: Any] = ["NAME": "Kimmy Stein", "HEIGHT": 41, "EXP": false, "GUARDIAN": "Bill and Hilary Stein"]
+let player7: [String: Any] = ["NAME": "Sammy Adams", "HEIGHT": 45, "EXP": false, "GUARDIAN": "Jeff Adams"]
+let player8: [String: Any] = ["NAME": "Karl Saygan", "HEIGHT": 42, "EXP": true, "GUARDIAN": "Heather Bledsoe"]
+let player9: [String: Any] = ["NAME": "Suzane Greenberg", "HEIGHT": 44, "EXP": true, "GUARDIAN": "Henrietta Dumas"]
+let player10: [String: Any] = ["NAME": "Sal Dali", "HEIGHT": 41, "EXP": false, "GUARDIAN": "Gala Dali"]
+let player11: [String: Any] = ["NAME": "Joe Kavalier", "HEIGHT": 39, "EXP": false, "GUARDIAN": "Sam and Elaine Kavalier"]
+let player12: [String: Any] = ["NAME": "Ben Finkelstein", "HEIGHT": 44, "EXP": false, "GUARDIAN": "Aaron and Jill Finkelstein"]
+let player13: [String: Any] = ["NAME": "Diego Soto", "HEIGHT": 41, "EXP": true, "GUARDIAN": "Robin and Sarika Soto"]
+let player14: [String: Any] = ["NAME": "Chloe Alaska", "HEIGHT": 47, "EXP": false, "GUARDIAN": "David and Jamie Alaska"]
+let player15: [String: Any] = ["NAME": "Arnold Willis", "HEIGHT": 43, "EXP": false, "GUARDIAN": "Claire Willis"]
+let player16: [String: Any] = ["NAME": "Phillip Helm", "HEIGHT": 44, "EXP": true, "GUARDIAN": "Thomas Helm and Eva Jones"]
+let player17: [String: Any] = ["NAME": "Les Clay", "HEIGHT": 42, "EXP": true, "GUARDIAN": "Wynonna Brown"]
+let player18: [String: Any] = ["NAME": "Herschel Krustofski", "HEIGHT": 45, "EXP": true, "GUARDIAN": "Hyman and Rachel Krustofski"]
 
 // One collection for all players data
 
@@ -43,21 +43,17 @@ var countSort = 1
 // Sorting players into has or hasnot experience containers loop
 while countSort <= counter {
 for object in players {
-    for (key, value) in object {
-    switch value {
-    case "Yes": hasExp.append(object)
-    case "No": noExp.append(object)
-    default: break}
-}
-    countSort += 1
+    let experience = object["EXP"] as! Bool
+    switch experience {
+    case true: hasExp.append(object)
+    case false: noExp.append(object)
+    default: break
     }
-    
 }
-
 /*
  ---------------------------------------------------
  Splitting both lists up evengly into the three teams
- ---------------------------------------------------
+w ---------------------------------------------------
  */
 
 // Empty array for three soccer teams
